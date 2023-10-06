95a8f0eb80d8c595750e596214939b569da7be9e
Bhargav Joshi <68766403+bhargavjoshi1237@users.noreply.github.com>
2023-10-06 14:31:35 -0700

    Add files via upload

---
+++ app/blog/page.js
@@ -0,0 +1,49 @@
+import 'C:/Users/Bhargav Joshi/Desktop/Next/animealley/app/globals.css'
+export default function () {
+    return(
+      <>
+      <div style={{width:"100%"}}>
+        <div  className='columns-2 top-0 right-0' ><h3 style={{color:"white", fontSize:"27px", paddingLeft:"5.5%",paddingTop:"30px", fontFamily:'Open'}}>Anime Alley</h3>
+       <img className='absolute top-0 right-0' style={{width:"42px",paddingTop:"28px",marginRight:"1.5%"}} src="https://img.icons8.com/windows/64/FFFFFF/user-male-circle.png" alt="" />
+         </div> <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-500"></hr>
+     
+       
+
+<div className='columns-2' style={{paddingLeft:"1.6%",}}>
+<div > <span style={{marginLeft:"3.5%",marginTop:"56px"}} class="bg-gray-100 text-gray-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded mr-2 dark:bg-gray-700 dark:text-gray-400 border border-gray-500">
+        <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-sm font-semibold text-blue-800 bg-blue-100 rounded-full dark:bg-gray-700 dark:text-blue-400">
+  <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+    <path fill="currentColor" d="m18.774 8.245-.892-.893a1.5 1.5 0 0 1-.437-1.052V5.036a2.484 2.484 0 0 0-2.48-2.48H13.7a1.5 1.5 0 0 1-1.052-.438l-.893-.892a2.484 2.484 0 0 0-3.51 0l-.893.892a1.5 1.5 0 0 1-1.052.437H5.036a2.484 2.484 0 0 0-2.48 2.481V6.3a1.5 1.5 0 0 1-.438 1.052l-.892.893a2.484 2.484 0 0 0 0 3.51l.892.893a1.5 1.5 0 0 1 .437 1.052v1.264a2.484 2.484 0 0 0 2.481 2.481H6.3a1.5 1.5 0 0 1 1.052.437l.893.892a2.484 2.484 0 0 0 3.51 0l.893-.892a1.5 1.5 0 0 1 1.052-.437h1.264a2.484 2.484 0 0 0 2.481-2.48V13.7a1.5 1.5 0 0 1 .437-1.052l.892-.893a2.484 2.484 0 0 0 0-3.51Z"/>
+    <path fill="#fff" d="M8 13a1 1 0 0 1-.707-.293l-2-2a1 1 0 1 1 1.414-1.414l1.42 1.42 5.318-3.545a1 1 0 0 1 1.11 1.664l-6 4A1 1 0 0 1 8 13Z"/>
+  </svg>
+  <span class="sr-only">Icon description</span>
+</span>
+  
+  YouTube NEWS
+</span><h1 style={{paddingTop:"25px",fontFamily:'Open',fontSize:"35px",paddingLeft:"3.5%",width:"100%"}}>Dillon Danis Denies Pulling Out of Logan Paul Fight After Tweeting About It.</h1></div>
+<p style={{width:"100%",color:"#707070",fontFamily:'Openr',paddingLeft:"3.5%",marginTop:"20px"}}>Over the years, MMA athlete Dillon Danis has built himself a reputation for never showing up to any of his fights. It happens every time, this strange self-sabotaging that always ends with him pulling out of a fight for one reason or another. During the most recent FLAGRANT podcast episode, comedian Andrew Schulz and his crew kept telling Danis he would probably pull out. But Dillon kept on telling them he was 100% committed to the fight. Thursday's tweet outburst after watching Logan Paul brag about the many lawsuits against Danis made him explode.</p>
+<p style={{width:"100%",color:"#707070",fontFamily:'Openr',paddingLeft:"3.5%",marginTop:"20px"}}>In his own words, he did say "I'm out!" But he backtracked after Mike Perry said he was officially taking Dillon Danis' spot. As expected, almost all of sports media had to write up a piece like this one to clarify the situation.
+In his own words, he did say "I'm out!" But he backtracked after Mike Perry said he was officially taking Dillon Danis' spot. As expected, almost all of sports media had to write up a and once lost, it's hard to recover.
+
+</p>
+<img className='shadow-inner' style={{maxWidth:"80%",marginLeft:"150px"}} src="https://cdn.vox-cdn.com/thumbor/0y-hyENf_E1C61JydjRSpTjS0TA=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/16346203/064_Dillon_Danis_x_Max_Humphrey.jpg" alt="" />
+
+</div>
+<hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-600"></hr>
+<div className='columns-4'>
+
+<img style={{paddingBottom:"10px"}} src="https://phantom-marca.unidadeditorial.es/fae5ffe6522c1fb5e485d683f48111f1/resize/828/f/jpg/assets/multimedia/imagenes/2023/10/05/16965367769281.jpg" alt="" />
+<img style={{paddingBottom:"10px"}} src="https://dotesports.com/wp-content/uploads/2023/09/Logan-Paul-vs-Dillon-Danis.jpg" alt="" />
+<img style={{paddingBottom:"10px"}} src="https://talksport.com/wp-content/uploads/sites/5/2023/08/Logan-Paul-1.png" alt="" />
+<img style={{paddingBottom:"10px"}} src="https://s.yimg.com/ny/api/res/1.2/.MalS6mzDD7G1U4WZxZVQA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://s.yimg.com/os/creatr-uploaded-images/2023-02/d336f7b0-b62a-11ed-a3fd-527abdfd96a3" alt="" />
+<img style={{paddingBottom:"10px"}} src="https://espnpressroom.com/us/files/2023/02/PaulvsFury-1920x1080.png" alt="" />
+<img style={{paddingBottom:"10px"}} src="https://www.insidesport.in/wp-content/uploads/2023/02/16558426346828.jpg" alt="" />
+<img src="https://talksport.com/wp-content/uploads/sites/5/2023/05/jj-olajide-olatunji-l-joe-817254019.jpg" alt="" />
+<br />
+<br />
+</div>
+</div>
+      
+</>
+    )
+} 
